85c0ee4d705d6a93f1267e4fab1ccfba8d4edab5
Thang Nguyen <tnguyen@solutiondigital.biz>
2023-12-10 12:32:57 +0700

    fix banner display on mobile

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image';
 
 function Banner() {
     return ( 
-        <div className="min-h-[400px] text-center flex items-center flex-col gap-4 relative pt-[80px]">
+        <div className="md:min-h-[400px] text-center flex items-center flex-col gap-4 relative">
             <Image src='/Jotun-5503-Natural-Blue.jpg' alt='' width={1440} height={1480} 
-                className='min-w-full min-h-[300px] max-h-[500px]'>
+                className='min-w-full min-h-[300px] max-h-[500px] w-full h-full'>
             </Image>
             <div className='absolute top-[45%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-center'>
                 <h1 className="hidden lg:block text-[36px] font-bold text-white w-[400px] mx-auto mb-4">Màu sắc chất lượng, Lựa chọn hoàn hảo</h1>
