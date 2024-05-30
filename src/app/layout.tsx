b5641298ea20a072f2eb6320310734e7e6db355f
Thang Nguyen <jessepinkman97@gmail.com>
2024-05-30 09:23:09 +0700

    added sitemap, robots, metadata tag, 404 page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,16 @@ import './globals.css';
 const playfairDisplay = Inter({ subsets: ['vietnamese'] })
 
 export const metadata: Metadata = {
-  title: 'Sơn Jotun Quang Tuyến',
+  title: 'Sơn Jotun Long Hậu',
   description: 'Chúng tôi tự hào giới thiệu đến bạn một nền tảng mua sắm trực tuyến hoàn hảo cho mọi nhu cầu sơn của bạn. Với hàng trăm loại sơn đa dạng và chất lượng từ các thương hiệu uy tín, chúng tôi cam kết mang đến cho bạn những sản phẩm tốt nhất để nâng tầm không gian sống và làm việc của bạn.',
+  applicationName: 'JotunLongHau',
+  keywords: ['sơn, sơn nội thất, sơn ngoại thất, jotun, sơn jotun, jotashield, majestic, sơn long hậu, sơn cần giờ, sơn quận 1',
+    'sơn quận 2', 'sơn quận 3', 'sơn quận 4', 'sơn quận 5', 'sơn quận 6', 'sơn quận 7', 'sơn quận 8', 'sơn quận 9', 'sơn quận gò vấp',
+    'sơn quận phú nhuận', 'sơn quận tân phú', 'sơn quận tân bình'
+  ],
+  viewport: "width=device-width, initial-scale=1",
+  robots: 'index, follow',
+  bookmarks: "https://jotunlonghau.com"
 }
 
 export default function RootLayout({
